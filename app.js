b98b65ce0684aaac45f2ed0e94fa00a482fa1bb6
AlQasimMall <mallalqasim@gmail.com>
2024-12-14 10:23:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const container = document.querySelector(".container");
 
 // بيانات القهوة مع روابط الصور
 const coffees = [
-  { name: "Perspiciatis", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Voluptatem", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Explicabo", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Rchitecto", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Beatae", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Vitae", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Inventore", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Veritatis", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
-  { name: "Accusantium", image: "https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png" },
+  { name: "Perspiciatis", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Voluptatem", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Explicabo", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Rchitecto", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Beatae", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Vitae", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Inventore", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Veritatis", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
+  { name: "Accusantium", image: "https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df" },
 ];
 
 // وظيفة لعرض القهوة
--- service-worker-js.js
+++ service-worker-js.js
@@ -5,7 +5,7 @@ const urlsToCache = [
   '/manifest.json',
   '/styles/main.css',
   '/scripts/main.js',
-  'https://github.com/AlQasimMall/Pasha-taxi/blob/main/Photoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png'
+  'https://firebasestorage.googleapis.com/v0/b/messageemeapp.appspot.com/o/driver-images%2FPhotoroom-%D9%A2%D9%A0%D9%A2%D9%A4%D9%A1%D9%A2%D9%A1%D9%A4_%D9%A0%D9%A9%D9%A4%D9%A6%D9%A5%D9%A1.png?alt=media&token=a8374a80-d851-4745-9a15-6e8cc5c432df'
 ];
 
 self.addEventListener('install', function(event) {
